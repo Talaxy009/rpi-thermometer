d89af4a1cf23871b09648b27293253849eddfbbd
Taozc <41264828+Talaxy009@users.noreply.github.com>
2020-10-11 17:02:10 +0800

    use SplitChunks

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,22 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  mode: 'production',
+  entry: './src/index.jsx',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, './web/asset/js')
   },
+  mode: 'production',
+  optimization:{
+    splitChunks:{
+      chunks: 'all',
+    },
+  },
   module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader'
-      }
-    ]
+    rules: [{
+      test: /\.(js|jsx)$/,
+      exclude: /(node_modules|bower_components)/,
+      loader: 'babel-loader'
+    }]
   }
 };
